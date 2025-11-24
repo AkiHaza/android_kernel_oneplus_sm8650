a09808f2bc320e02aabea8092e9b511537dc5a68
John Galt <johngaltfirstrun@gmail.com>
2025-11-24 15:10:45 -0500

    Reapply "timers: Rename del_timer() to timer_delete()"
    This reverts commit 2587385ace149623e3122717dc5707f21a697d48.

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1304,7 +1304,7 @@ out_unlock:
 EXPORT_SYMBOL_GPL(add_timer_on);
 
 /**
- * del_timer - Deactivate a timer.
+ * timer_delete - Deactivate a timer
  * @timer:	The timer to be deactivated
  *
  * The function only deactivates a pending timer, but contrary to
@@ -1317,7 +1317,7 @@ EXPORT_SYMBOL_GPL(add_timer_on);
  * * %0 - The timer was not pending
  * * %1 - The timer was pending and deactivated
  */
-int del_timer(struct timer_list *timer)
+int timer_delete(struct timer_list *timer)
 {
 	struct timer_base *base;
 	unsigned long flags;
@@ -1333,7 +1333,7 @@ int del_timer(struct timer_list *timer)
 
 	return ret;
 }
-EXPORT_SYMBOL(del_timer);
+EXPORT_SYMBOL(timer_delete);
 
 /**
  * try_to_del_timer_sync - Try to deactivate a timer
